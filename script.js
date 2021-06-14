32e562478870e661101771560e92dcb1e837ebe0
ecruz17 <ecruz17@ucol.mx>
2021-06-13 20:35:34 -0500

    updated style & implemented decimals input

--- script.js
+++ script.js
@@ -5,8 +5,9 @@ function calculate() {
     var x0 = $('#val-x0').val();
     var x1 = $('#val-x1').val();
     var fx0 = $('#val-fx0').val();
-    //var fx1 = $('#val-fx1').val();
     var fx1 = ((3*1) + Math.sin(1)) - Math.pow(euler, 1);
+    var decimales = $('#val-decimals').val();
+    
     
     document.querySelector('#val-fx1').value = fx1;
 
@@ -19,7 +20,7 @@ function calculate() {
     //let ep = ((res - 2.61538)/res) * 100
 
 
-    if (x0 != "" && x1 != "" && fx0 != "" && fx1 != "") {
+    if (x0 != "" && x1 != "" && fx0 != "" && fx1 != "" && decimales != "") {
         //Guarda las variables introducidas en el registro de x0 y fx0
         regX0.push(x1);
         regFx0.push(fx1);
@@ -28,10 +29,10 @@ function calculate() {
         let counter = 1;
 
         let x = x1 - ((fx1*(x0-x1))/(fx0-fx1));
-        //x = this._truncate(x);
+        x = this._truncate(x);
             
         let fx2 = ((3*x) + Math.sin(x)) - Math.pow(euler, x);
-        //fx2 = _truncate(fx2);
+        fx2 = _truncate(fx2);
 
         //Muestra la iteración 1
         this._showTable(counter, x, fx2);
@@ -53,16 +54,17 @@ function calculate() {
             regX0.push(x0);
             regFx0.push(fx0);
 
+            /* Puebas en consola
             console.log(x0);
             console.log(fx0);
             console.log(x1);
-            console.log(fx1);
+            console.log(fx1); */
 
             x = x1 - ((fx1*(x0-x1))/(fx0-fx1));
-            //x = this._truncate(x);
+            x = this._truncate(x);
                 
             fx2 = ((3*x) + Math.sin(x)) - Math.pow(euler, x);
-            //fx2 = _truncate(fx2);
+            fx2 = _truncate(fx2);
 
             regX0.push(x0);
             regFx0.push(fx0);
@@ -84,7 +86,8 @@ function calculate() {
 }
 
 function _truncate (num) {
-    var multiplier = Math.pow(10, 4),
+    var decimales = $('#val-decimals').val();
+    var multiplier = Math.pow(10, decimales),
     adjustedNum = num * multiplier,
     truncatedNum = Math[adjustedNum < 0 ? 'ceil' : 'floor'](adjustedNum);
     return num = truncatedNum / multiplier;
@@ -97,6 +100,8 @@ function clean() {
     $('#val-x1').val("");
     $('#val-fx0').val("");
     $('#val-fx1').val("");
+    $('#val-decimals').val("");
+    this._showTable('', '', '');
 
 }
 
